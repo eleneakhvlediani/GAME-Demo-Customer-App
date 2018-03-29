346c09235f5e7374af8afaaaa39d791d5b282df2
Salome Tsiramua <salome.tsiramua@gmail.com>
2018-03-30 00:24:33 +0400

    NFC added to app

---
+++ Game customer App/Game customer App/NFCReader.swift
@@ -0,0 +1,39 @@
+//
+//  NFCReader.swift
+//  Game customer App
+//
+//  Created by Salome Tsiramua on 3/29/18.
+//  Copyright © 2018 Elene Akhvlediani. All rights reserved.
+//
+import CoreNFC
+
+
+import Foundation
+
+
+class NFCReader: NSObject, NFCNDEFReaderSessionDelegate {
+
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                print("New Record Found:")
+                print(record.identifier)
+                print(record.payload)
+                print(record.type)
+                print(record.typeNameFormat)
+            }
+        }
+    }
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("NFC NDEF Invalidated")
+        print("\(error)")
+    }
+
+
+    func beginSession() {
+        let session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session.begin()
+    }
+
+}
--- Game customer App/Game customer App/ViewController.swift
+++ Game customer App/Game customer App/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func startSearch(_ sender: UIButton) {
+        
+        nfcReader.beginSession()
+    }
+    let nfcReader = NFCReader()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
 
 }
 
